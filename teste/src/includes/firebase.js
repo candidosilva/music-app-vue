4d07c278d6e8ab51d8afcdab13c9ff56e3812a19
Cândido da Silva Junior <57716228+candidosilva@users.noreply.github.com>
2023-01-28 07:41:33 -0300

    Music app project
    Music app project

---
+++ teste/src/includes/firebase.js
@@ -0,0 +1,37 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCaotDV52XxPb4UN4hNNfxQNvOCWgN667s",
+  authDomain: "music-81837.firebaseapp.com",
+  projectId: "music-81837",
+  storageBucket: "music-81837.appspot.com",
+  messagingSenderId: "670444969181",
+  appId: "1:670444969181:web:201e84c4fe0f5b3f434f34",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
+
+db.enablePersistence().catch((error) => {
+  console.log(`Firebase persistence error ${error.code}`);
+});
+
+const usersCollection = db.collection("users");
+const songsCollection = db.collection("songs");
+const commentsCollection = db.collection("comments");
+
+export {
+  auth,
+  db,
+  usersCollection,
+  songsCollection,
+  commentsCollection,
+  storage,
+};
